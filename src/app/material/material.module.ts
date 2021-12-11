8591938e6617d51b95ec3b02f10b3e9ba8fd6937
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-11 15:37:02 -0600

    algunos cambios

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
--- src/app/personajes/personajes.module.ts
+++ src/app/personajes/personajes.module.ts
@@ -1,14 +1,20 @@
-//! modulos de angular
+//! Angular Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-//* componentes creados
+//? Material Modules
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+//? Modulos
+import { personajesRoutingModule } from './personajes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
+//* componentes
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { PersonajeComponent } from './pages/personaje/personaje.component';
 import { PersonajesHomeComponent } from './pages/personajes-home/personajes-home.component';
-import { personajesRoutingModule } from './personajes-routing.module';
 
 
 
@@ -22,7 +28,9 @@ import { personajesRoutingModule } from './personajes-routing.module';
   ],
   imports: [
     CommonModule,
-    personajesRoutingModule
+    FlexLayoutModule,
+    personajesRoutingModule,
+    MaterialModule
   ]
 })
 export class PersonajesModule { }
