e98b6cac617b1191b29b36b92d383fffc82b7541
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-13 15:48:21 -0600

    servicios añadidos

---
+++ src/app/personajes/services/vtubers.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Vtuber } from '../interfaces/hololive.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VtubersService {
+
+  constructor( private http:HttpClient ) { }
+
+  getPersonajes():Observable<Vtuber[]> {
+    return this.http.get<Vtuber[]>('http://localhost:3000/personajes');
+  }
+
+}
