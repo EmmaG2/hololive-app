50004f3849b59ac9a0537ce959a2e9e778326311
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-13 15:50:56 -0600

    modulos importados

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   exports: [
     MatSidenavModule,
@@ -12,6 +13,7 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
