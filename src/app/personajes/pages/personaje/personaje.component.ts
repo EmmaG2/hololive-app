159acb0121a6bbfd9a1852ffbc021e63c462acf6
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-14 14:29:39 -0600

    personaje component creado

--- src/app/personajes/pages/personaje/personaje.component.ts
+++ src/app/personajes/pages/personaje/personaje.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Vtuber } from '../../interfaces/hololive.interface';
+import { VtubersService } from '../../services/vtubers.service';
 
 @Component({
   selector: 'app-personaje',
   templateUrl: './personaje.component.html',
-  styles: [
-  ]
+  styleUrls: ['./personaje.component.css']
 })
 export class PersonajeComponent implements OnInit {
 
-  constructor() { }
+  pers!:Vtuber;
+
+  constructor( private activatedRoute:ActivatedRoute,
+               private vtuberService:VtubersService,
+               private router:Router) { }
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params
+      .pipe( switchMap( ({id}) => this.vtuberService.getPersonajePorId(id) ) )
+      .subscribe( pers => this.pers = pers );
+  };
+
+  regresar() {
+    this.router.navigate(['personajes/listado'])
+  };
 
 }
