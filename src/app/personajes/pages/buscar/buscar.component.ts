7674fe0aa1689d2e4b746c0085e4e73061e4c0a7
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-15 15:51:28 -0600

    componente buscar creado

--- src/app/personajes/pages/buscar/buscar.component.ts
+++ src/app/personajes/pages/buscar/buscar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Vtuber } from '../../interfaces/hololive.interface';
+import { VtubersService } from '../../services/vtubers.service';
 
 @Component({
   selector: 'app-buscar',
@@ -7,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BuscarComponent implements OnInit {
+  termino: string = '';
+  dosb: Vtuber[] = [];
+  personajeSeleccionado?:Vtuber | undefined;
 
-  constructor() { }
+  constructor(private vtubersService: VtubersService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  buscando() {
+    this.vtubersService
+      .getSugerencias(this.termino.trim())
+      .subscribe((personajes) => (this.dosb = personajes));
+  };
+
+  opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+
+    if (!event.option.value) {
+      return;
+    }
+
+    const vtuber:any = event.option.value;
+    this.termino = vtuber.vtuber.name.en;
+
+    this.vtubersService.getPersonajePorId( vtuber.id )
+    .subscribe( personaje => this.personajeSeleccionado = personaje)
+
   }
 
 }
