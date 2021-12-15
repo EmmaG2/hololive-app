7f81355f3cc691e33786cda28b5709df3f9149b9
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-12-15 15:50:55 -0600

    más modulos de material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
+    MatAutocompleteModule,
     MatButtonModule,
-    MatIconModule,
-    MatListModule,
     MatCardModule,
     MatGridListModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatListModule,
     MatProgressSpinnerModule,
+    MatSidenavModule,
   ],
 })
 export class MaterialModule {}
